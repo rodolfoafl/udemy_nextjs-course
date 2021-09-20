0a3ad1a740a765e8bcf617e5071be43c8633e565
Rodolfo <27327749+rodolfoafl@users.noreply.github.com>
2021-09-19 23:24:24 -0300

    Events Functionality - Part 1

---
+++ dj-events-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9d0e5251e3fdc9183f833944c1ef4633'),
+    },
+  },
+});
---
+++ dj-events-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5c9163a9-4b1e-450a-89a1-f63d8dd86094'
+};
