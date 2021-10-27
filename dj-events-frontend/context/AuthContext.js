67e57a0e7d5a645c4bdddeb61f72a5852ef06c65
Rodolfo Leal <27327749+rodolfoafl@users.noreply.github.com>
2021-10-26 22:31:04 -0300

    Implemented JWT token on login/logout

--- dj-events-frontend/context/AuthContext.js
+++ dj-events-frontend/context/AuthContext.js
@@ -1,6 +1,6 @@
 import { createContext, useState, useEffect } from "react";
 import  {useRouter} from 'next/router';
-import { API_URL } from "../config";
+import { NEXT_URL } from "../config";
 
 const AuthContext = createContext();
 
@@ -8,20 +8,61 @@ export const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null);
     const [error, setError] = useState(null);
 
+    const router = useRouter();
+
+    useEffect(() =>{
+        checkUserLoggedIn();
+    }, [])
+
     const register = async (user) => {
         console.log(user);
     } 
 
     const login = async ({email: identifier, password}) =>{
-        console.log({identifier, password});
+        const res = await fetch(`${NEXT_URL}/api/login`,{
+            method: 'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({
+                identifier,
+                password
+            })
+        })
+
+        const data = await res.json();
+        console.log(data);
+
+        if(res.ok){
+            setUser(data.user);
+
+            router.push('/account/dashboard');
+        }else{
+            setError(data.message);
+            setError(null);
+        }
     }
 
     const logout = async () => {
-        console.log('logout');
+        const res = await fetch(`${NEXT_URL}/api/logout`, {
+            method: 'POST',
+        })
+
+        if(res.ok){
+            setUser(null);
+            router.push('/')
+        }
     }
 
     const checkUserLoggedIn = async (user) => {
-        console.log('check');
+        const res = await fetch(`${NEXT_URL}/api/user`);
+        const data = await res.json();
+
+        if(res.ok){
+            setUser(data.user);
+        }else{
+            setUser(null);
+        }
     }
 
     return (
